0bb998b31f8723b43a72013a38f4efcd2172be8a
Aidan <aidanshimokawa21@gmail.com>
2023-01-24 10:13:01 +0800

    added console log

---
+++ Index.js
@@ -0,0 +1,12 @@
+// backend = hard to learn easy to master 
+// frontend = easy to learn, hart to master
+//packages- are "add ons"
+// express - 
+// first get the ssh id
+// second create the repository - "file" on github
+// 3rd git add the file/s
+// git add name and email
+// git commit - "checkpoint" (-m)-required
+//git push 
+// done
+console.log("text")
